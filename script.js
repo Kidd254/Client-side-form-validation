5f1377ae686c59a3c274ca7d6bab15d700893d9d
Kidd254 <lorenkioko@gmail.com>
2023-05-22 10:08:24 +0300

    modify html and js files

--- script.js
+++ script.js
@@ -39,11 +39,52 @@ function validateEmail(emailInput){
     }
 }
 
-window.onload = function(){
-    if(localStorage) {
-        formField('submit', function(){
-const fullName = document.getElementById('name').value;
-localStorage.setItem('name', name);
-        });
-    }
+//Data Preservation
+let dataForm = {
+    name: "",
+    email: "",
+    message: "",
+}
+
+//dataForm=JSON.parse(localStorage.getItem('info'));
+const userName= document.getElementById('name')
+const userMail= document.getElementById('email')
+const userMessage= document.getElementById('text')
+
+userName.addEventListener('input', ()=> {
+dataForm.name= userName.value;
+localStorage.setItem('name', JSON.stringify(dataForm))
+});
+
+userMail.addEventListener('input', ()=>{
+dataForm.email= userMail.value;
+localStorage.setItem('email', JSON.stringify(dataForm));
+});
+
+userMessage.addEventListener('input', ()=>{
+dataForm.message= userMessage.value;
+localStorage.setItem('message', JSON.stringify(dataForm));
+});
+
+if (localStorage.getItem('info') !== null) {
+  console.log('Data has been stored locally.');
+} else {
+  console.log('Data has not been stored locally.');
+}
+
+//retrieval
+if(localStorage.getItem('info')){
+    let objectForm = localStorage.getItem('info');
+    objectForm= JSON.parse(objectForm);
+ document.getElementById('name').value= objectForm.name;
+  document.getElementById('email').value= objectForm.email 
+   document.getElementById('text').value=objectForm.message;
+
+}
+
+
+if (localStorage.getItem('info') !== null) {
+  console.log('Data has been stored locally.');
+} else {
+  console.log('Data has not been stored locally.');
 }
